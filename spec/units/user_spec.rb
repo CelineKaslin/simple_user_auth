a708a630ea8e6ab976ef5eeac15d37e6721e2cfc
celine kaslin <celine.kaslin@gmail.com>
2019-01-08 15:27:10 +0000

    unit tests updated and passing

--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -6,5 +6,14 @@ describe User do
    expect(User.authenticate("test@gmail.com", "password")).to eq (nil)
  end
 
+ it "Should return nil if user exists but password is incorrect" do
+   user = User.create(email: "something@gmail.com", password: 3)
+   expect(User.authenticate("something@gmail.com", "somethingelse")).to eq (nil)
+ end
+
+ it "Should return user if user exists" do
+   user = User.create(email: "something@gmail.com", password: "secret")
+   expect(User.authenticate("something@gmail.com", "secret")).to eq (user)
+ end
 
 end
